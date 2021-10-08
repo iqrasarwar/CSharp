4bf2af57bfddbd2637dd0f1df45e9eaa30602e35
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2021-10-08 08:39:55 +0500

    Update codeSnippets.cs

--- codeSnippets.cs
+++ codeSnippets.cs
@@ -1,1 +1,15 @@
-
+                                                          //input and output
+            //output
+            Console.WriteLine("what's your name?");
+            //input
+            string name = Console.ReadLine();
+            //output
+            Console.WriteLine("what's your age?");
+            //input
+            string age = Console.ReadLine();
+            //output
+            Console.WriteLine("what's your cgpa?");
+            //input
+            string cgpa = Console.ReadLine();
+            //interpolation string
+            Console.WriteLine($"{name} you are {age} years old with cgpa {cgpa}.");
