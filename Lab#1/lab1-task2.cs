81006edd569749fc1434f4560812d0cd8b9cd7ef
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2021-10-13 21:26:45 +0500

    create sendEmail() method

--- Lab#1/lab1-task2.cs
+++ Lab#1/lab1-task2.cs
@@ -1,1 +1,16 @@
+using System;
 
+namespace Task2
+{
+    class Program
+    {
+        public static void sendEmail(String Message, string to, String cc = "", bool attachment = false)
+        {
+            Console.WriteLine("{0} {1}", Message, to);
+        }
+        static void Main(string[] args)
+        {
+            sendEmail("This is message", "to abc@gmail.com.");
+        }
+    }
+}
