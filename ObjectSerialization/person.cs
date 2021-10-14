07756a277d54ae87c376bdab6f2caf90bc16b6dc
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2021-10-15 00:15:14 +0500

    Create person.cs

---
+++ ObjectSerialization/person.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace ConsoleApp3
+{
+    public class Person
+    {
+        private int age;
+        private int cgpa;
+
+        public int Cgpa
+        {
+            get { return cgpa; }
+            set { cgpa = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Cgpa: {this.cgpa} , Age: {this.age}";
+        }
+
+        public void WritePersons(List<Person> p)
+        {
+            StreamWriter sw = new StreamWriter("file.txt");
+            foreach (Person person in p)
+            {
+                sw.WriteLine(JsonSerializer.Serialize(person));
+            }
+            sw.Close();
+        }
+        public List<Person> PrintPersonsToConsole()
+        {
+            List<Person> p = new List<Person>();
+            StreamReader sr = new StreamReader("file.txt");
+            string s = string.Empty;
+            while ((s = sr.ReadLine()) != null)
+            {
+                Person per = JsonSerializer.Deserialize<Person>(s);
+                p.Add(per);
+            }
+            sr.Close();
+            return p;
+        }
+    }
+}
