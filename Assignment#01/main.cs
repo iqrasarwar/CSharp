702e5c773be52cc5c30fd69342ed8939a3c6a6cb
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2021-10-22 23:02:13 +0500

    Update main.cs

--- Assignment#01/main.cs
+++ Assignment#01/main.cs
@@ -1,1 +1,18 @@
 
+//BSEF19M012-IQRA SARWAR
+using EmsInterface;
+
+namespace Main_Proj
+{
+    class MainP
+    {
+        /// <summary>
+        /// Thia is entry point of program.It starts the interface for the Employee Managment System.
+        /// </summary>
+        /// <param name="args">command line arguments if any passed</param>
+        static void Main(string[] args)
+        {
+            EMS_Interface.startEMS();
+        }
+    }
+}
