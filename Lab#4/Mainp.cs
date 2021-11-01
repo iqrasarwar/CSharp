065bd8c186b5b23674defa6c4cff44d036680e65
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2021-11-01 20:42:44 +0500

    Update Mainp.cs

--- Lab#4/Mainp.cs
+++ Lab#4/Mainp.cs
@@ -1,1 +1,16 @@
 
+//bsef19m012 iqra sarwar
+using DonationManagment;
+namespace DB
+{
+    class MainP
+    {
+        /// <summary>
+        /// Launching the BLOOD DONATION MANAGMENT SYSTEM by an interface
+        /// </summary>
+        static void Main(string[] args)
+        {
+            DonationClassManagment.DisplayMenu();
+        }
+    }
+}
